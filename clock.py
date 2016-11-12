75df8510c6399977a40e965722283dc0c15fbea9
YinghuiYang <yinghui.yang@vanderbilt.edu>
2016-11-12 15:49:01 -0600

    filled in code with python and wrote in bash commands

---
+++ clock.py
@@ -0,0 +1,9 @@
+import datetime
+import time
+
+running = True
+while running:
+	current_time = datetime.datetime.now()
+	# Fill in code here to print the current time
+	time.sleep(1)
+	running = False
